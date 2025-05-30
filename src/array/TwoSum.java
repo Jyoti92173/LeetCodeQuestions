8a8b96eff8f6dd209662afbac477b3af5a4f6a8a
jyoti92173 <Jyoti53913@gmail.com>
2025-05-30 16:34:42 +0530

    Implemented TwoSum Solution

---
+++ src/array/TwoSum.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Arrays;
+
+public class TwoSum {
+        public int[] twoSum(int[] nums, int target) {
+            int n = nums.length;
+            for(int i = 1; i<n; i++){
+                for(int j = i; j<n; j++){
+                    if(nums[j]+nums[j-i] == target){
+                        return new int[] {j-i,j};
+                    }
+                }
+            }
+            return new int [] {};
+
+        }
+        public static void main(String []args){
+          int []nums = {2,7,11,15};
+          int target = 9;
+          TwoSum ts = new TwoSum();
+          System.out.println(Arrays.toString(ts.twoSum(nums, target)));
+        }
+    }
