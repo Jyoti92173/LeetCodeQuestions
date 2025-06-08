a985aff85c89b63f1e3a73be254927181ca8337a
jyoti92173 <Jyoti53913@gmail.com>
2025-06-08 16:11:53 +0530

    Implemented GroupAnagrams Solution

---
+++ src/array/GroupAnagrams.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            map.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+    public static void main(String[]args){
+        String strs [] = {"eat","tea","tan","ate","nat","bat"};
+        GroupAnagrams groupAnagram = new GroupAnagrams();
+        System.out.println(groupAnagram.groupAnagrams(strs));
+    }
+}
