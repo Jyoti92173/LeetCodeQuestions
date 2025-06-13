ab90c9ec7c601879c94acde7fd37a3119cf28d1d
jyoti92173 <Jyoti53913@gmail.com>
2025-06-13 22:56:45 +0530

    Implemented another MovesZeroes Solution.

--- src/array/MoveZeroes.java
+++ src/array/MoveZeroes.java
@@ -16,10 +16,29 @@ public class MoveZeroes {
         }
         return false;
     }
+    public boolean moveZeroes2(int[] nums) {
+        int m = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                if (m == -1 || nums[m] != 0) {
+                    m = i;
+                }
+            } else {
+                if (m != -1) {
+                    int temp = nums[i];
+                    nums[i] = nums[m];
+                    nums[m] = temp;
+                    m++;
+                }
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         int[] nums = {0,1,0,3,12};
         MoveZeroes zeroes = new MoveZeroes();
         System.out.println(zeroes.moveZeroes(nums));
+        System.out.println(zeroes.moveZeroes2(nums));
     }
 }
