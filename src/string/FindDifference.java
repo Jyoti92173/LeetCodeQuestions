f967077da8b627e0926571a76a2ca7ff9de5046b
jyoti92173 <Jyoti53913@gmail.com>
2025-06-14 13:00:44 +0530

    Implemented Another Solution of FindDifference.

--- src/string/FindDifference.java
+++ src/string/FindDifference.java
@@ -12,10 +12,21 @@ public class FindDifference {
         }
         return (char)(sum2-sum1);
     }
+    public static char findTheDifference2(String s1, String s2) {
+        int sum1 = 0,sum2 = 0;
+        for(char c:s2.toCharArray()){
+            sum1^=c;
+        }
+        for(char c:s1.toCharArray()){
+            sum2^=c;
+        }
+        return (char)(sum1^sum2);
+    }
     public static void main(String[] args) {
         String s1 = "abcd";
         String s2 = "abcde";
         FindDifference difference = new FindDifference();
         System.out.println(difference.findTheDifference(s1,s2));
+        System.out.println(difference.findTheDifference2(s1,s2));
     }
 }
